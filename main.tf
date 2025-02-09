1961a8b12f572b7cfd8a5dd511582af0bd543cae
My Name <myemail@example.com>
2025-02-09 13:30:30 +0530

    changed the resource

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   subscription_id = "362c226d-ca9d-48db-bbec-9ab0d6c3e55a"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "examples" {
+  name     = "example-resources1"
   location = "East US"
 }
